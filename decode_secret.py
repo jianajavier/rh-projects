a21630c32aa562e452bcccacfd07e6efb5077339
Daniel Soukhov <dsoukhov@unused-10-15-17-220.yyz.redhat.com>
2016-05-25 13:10:51 -0400

    decode now ignores lines starting with #

--- decode_secret.py
+++ decode_secret.py
@@ -7,9 +7,15 @@ parser = argparse.ArgumentParser(description='Enter arguments')
 parser.add_argument("f", nargs=1, type=str)
 
 args = parser.parse_args()
+data_json = ''
 
-with open(args.f[0]) as data_json:
-    data = json.load(data_json)["data"]
+
+with open(args.f[0]) as f:
+    for line in f:
+        if not line.strip().startswith("#"):
+            data_json += line
+
+data = json.loads(data_json)["data"]
 
 for filename, b64data in data.iteritems():
     print(filename+"\n"+"-----"+"\n"+base64.b64decode(b64data))
